eb55bbc401b170fea3aef07d3f212b6f31b03e3b
Jumps0 <cody@krselectric.com>
2024-04-15 13:39:10 -0700

    Test Build

--- Cogworld/Assets/Resources/Scripts/Machines/Fabricator.cs
+++ Cogworld/Assets/Resources/Scripts/Machines/Fabricator.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
-using static UnityEditor.Progress;
 
 public class Fabricator : MonoBehaviour
 {
