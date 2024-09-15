a19fcedd29fe208e17e389a88eaa44b71dee5dbc
Jumps0 <49619518+Jumps0@users.noreply.github.com>
2024-09-15 12:01:27 +0200

    Makes the SObject sprite visualizer more generic to work with other TileObjects

--- Cogworld/Assets/Editor/SOSprites.cs
+++ Cogworld/Assets/Editor/SOSprites.cs
@@ -3,36 +3,33 @@
 using UnityEditor;
 using UnityEngine;
 
-//We connect the editor with the Weapon SO class
-[CustomEditor(typeof(MachineTile))]
-//We need to extend the Editor
+[CustomEditor(typeof(TileObject), true)]
 public class SOSPrites : Editor
 {
-    //Here we grab a reference to our Weapon SO
+    // Reference to a general TileObject
     TileObject tile;
 
     private void OnEnable()
     {
-        //target is by default available for you
-        //because we inherite Editor
         tile = target as TileObject;
     }
 
-    //Here is the meat of the script
     public override void OnInspectorGUI()
     {
-        //Draw whatever we already have in SO definition
         base.OnInspectorGUI();
-        //Guard clause
-        if (tile.displaySprite == null)
+        if (tile == null || tile.displaySprite == null)
             return;
 
-        //Convert the tileSprite (see SO script) to Texture
+        // Display the sprite's name above the preview
+        GUILayout.Label($"Sprite: {tile.displaySprite.name}");
+
+        // Convert the tileSprite (see SO script) to Texture
         Texture2D texture = AssetPreview.GetAssetPreview(tile.displaySprite);
-        //We crate empty space 80x80 (you may need to tweak it to scale better your sprite
-        //This allows us to place the image JUST UNDER our default inspector
+
+        // Create an empty space for the sprite preview (you may tweak dimensions)
         GUILayout.Label("", GUILayout.Height(80), GUILayout.Width(80));
-        //Draws the texture where we have defined our Label (empty space)
+
+        // Draws the texture where we have defined our Label (empty space)
         GUI.DrawTexture(GUILayoutUtility.GetLastRect(), texture);
     }
 }
